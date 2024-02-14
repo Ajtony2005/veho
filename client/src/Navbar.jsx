308c1dccd9ac5276357cc4c35538deb8fc134376
Ajtony2005 <ajtony.szauter@gmail.com>
2024-02-15 00:28:09 +0100

    Navbar sokat feljlödött

--- client/src/Navbar.jsx
+++ client/src/Navbar.jsx
@@ -6,11 +6,15 @@ import { HU, GB } from 'country-flag-icons/react/3x2';
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <ul>
-        <img src={process.env.PUBLIC_URL + '/logo.png'} alt="VEHO Logo" className="logo" style={{ width: '80px', height: 'auto' }} />
-        <li><HU title="Magyarország" style={{ width: '40px', height: 'auto' }} /></li>
-        <li><GB title="United Kingdom" style={{ width: '40px', height: 'auto' }} /></li>
-        <li><SignInButton /></li>
+      <ul className="navbar-items">
+        <img src={process.env.PUBLIC_URL + '/logo.png'} alt="VEHO Logo" className="logo" />
+        <div className="navbar-right">
+          <ul className="navbar-items-right">
+            <li><HU title="Magyarország" style={{ width: '40px', height: 'auto' }} /></li>
+            <li><GB title="United Kingdom" style={{ width: '40px', height: 'auto' }} /></li>
+            <li><SignInButton /></li>
+          </ul>
+        </div>
       </ul>
     </nav>
   );
