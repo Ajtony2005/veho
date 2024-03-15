25a1d3f24e2affa12889d7a4a7dacf7aaf144c2c
Ajtony2005 <ajtony.szauter@gmail.com>
2024-03-16 00:50:23 +0100

    szöveg doboz és gombok beállítása

--- client/src/Information.jsx
+++ client/src/Information.jsx
@@ -1,5 +1,9 @@
 import React from 'react';
 import './Information.css'; // Style fájl importálása
+import { Box } from '@mui/material'; 
+import { IconButton } from '@mui/material';
+import { ArrowBack, ArrowForward } from '@mui/icons-material';
+
 
 const CircleImage = ({ imageUrl }) => {
   return (
@@ -7,8 +11,18 @@ const CircleImage = ({ imageUrl }) => {
       <div className="circle"></div>
       <img className="image" src={imageUrl} alt="Circle" />
       <div className="text-container">
+        <Box className="box">
         <h2>Cím</h2>
         <p>Folyamatos szöveg...</p>
+        <div className="icon-container">
+                    <IconButton>
+                      <ArrowBack />
+                    </IconButton>
+                    <IconButton>
+                      <ArrowForward />
+                    </IconButton>
+                  </div>
+        </Box>
       </div>
     </div>
   );
