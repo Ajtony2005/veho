a0259826b2dd08c2e2a492d300dfadf12ec4cee3
Ajtony2005 <79107320+Ajtony2005@users.noreply.github.com>
2024-06-03 22:58:01 +0200

    refactor: Update SignInButton component to handle language changes

--- client/src/Home/jsx/SignInButton.jsx
+++ client/src/Home/jsx/SignInButton.jsx
@@ -1,18 +1,42 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import LoginIcon from '@mui/icons-material/Login';
+import text from '../json/signin.json';
+import Cookies from 'js-cookie';
+
 
 const SignInButton = () => {
   const [clicked, setClicked] = useState(false);
+  const [language, setLanguage] = useState('hu'); 
+  useEffect(() => {
+    const storedLanguage = Cookies.get('language');
+    if (storedLanguage) {
+        setLanguage(storedLanguage);
+    }
+
+    const interval = setInterval(() => {
+        const currentLanguage = Cookies.get('language');
+        if (currentLanguage && currentLanguage !== language) {
+            setLanguage(currentLanguage);
+        }
+    }, 1000); 
+
+    return () => clearInterval(interval);
+}, [language]);
+
+const getText = (key) => {
+    return text[language][key];
+  };
+
+
 
   const handleClick = () => {
     setClicked(!clicked);
-    // Ide írd bele a további műveleteket, amelyeket szeretnél végrehajtani a gombra kattintás után
   };
 
   return (
     <button className="btn btn-outline-warning btn-sm btn-block" onClick={handleClick}>
-      <LoginIcon /> Bejelentkezés
+      <LoginIcon /> {getText('text')}
     </button>
   );
 };
