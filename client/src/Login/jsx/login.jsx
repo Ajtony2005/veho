ff9b082c2e9e7151c6954951690a93e4c89ee36c
Ajtony2005 <79107320+Ajtony2005@users.noreply.github.com>
2024-06-16 23:22:37 +0200

    chore: Update npm dependencies for FontAwesome and React Router Dom

--- client/src/Login/jsx/login.jsx
+++ client/src/Login/jsx/login.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from 'react';
 import '../css/login.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faGoogle, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { faGoogle, faFacebook, faGithub   } from '@fortawesome/free-brands-svg-icons';
+import { faEye, faEyeSlash  } from '@fortawesome/free-solid-svg-icons'
+
+
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [showPassword, setShowPassword] = useState(false);
 
     const handleUsernameChange = (e) => {
         setUsername(e.target.value);
@@ -29,53 +33,62 @@ const Login = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        // Implement your default login logic here
         console.log('Felhasználónév:', username);
         console.log('Jelszó:', password);
     };
 
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword);
+    };
+
     return (
         <div className="body-login">
-        <div className="login-container">
-            <div className="login-box">
-                <form onSubmit={handleSubmit}>
-                    <h1>Bejelentkezés</h1>
-                    <div>
-                    
-                        <input
-                            type="text"
-                            id="username"
-                            placeholder="Felhasználónév"
-                            className="login-input"
-                            value={username}
-                            onChange={handleUsernameChange}
-                        />
-                    </div>
-                    <div>
-                        <input
-                            type="password"
-                            id="password"
-                            placeholder="Jelszó"
-                            className="login-input"
-                            value={password}
-                            onChange={handlePasswordChange}
-                        />
-                    </div>
-                    <button type="submit" className="login-button" >Bejelentkezés</button>
-                    <div class="login-buttons">
-                    <button type="button" onClick={handleGoogleLogin} className="login-button-google">
-                        <FontAwesomeIcon icon={faGoogle} /> 
-                    </button>
-                    <button type="button" onClick={handleFacebookLogin} className="login-button-facebook">
-                        <FontAwesomeIcon icon={faFacebook} /> 
-                    </button>
-                    <button type="button" onClick={handleGitHubLogin} className="login-button-github">
-                        <FontAwesomeIcon icon={faGithub} /> 
-                    </button>
-                    </div>
-                </form>
+            <div className="login-container">
+                <div className="login-box">
+                    <form onSubmit={handleSubmit}>
+                        <h1>Bejelentkezés</h1>
+                        <div>
+                            <input
+                                type="text"
+                                id="username"
+                                placeholder="Felhasználónév"
+                                className="login-input"
+                                value={username}
+                                onChange={handleUsernameChange}
+                            />
+                        </div>
+                        <div className="password-container">
+                            <input
+                                type={showPassword ? "text" : "password"}
+                                id="password"
+                                placeholder="Jelszó"
+                                className="login-input"
+                                value={password}
+                                onChange={handlePasswordChange}
+                            />
+                            <button
+                                type="button"
+                                className="password-toggle-button"
+                                onClick={togglePasswordVisibility}
+                            >
+                                <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
+                            </button>
+                        </div>
+                        <button type="submit" className="login-button">Bejelentkezés</button>
+                        <div className="login-buttons">
+                            <button type="button" onClick={handleGoogleLogin} className="login-button-google">
+                                <FontAwesomeIcon icon={faGoogle} />
+                            </button>
+                            <button type="button" onClick={handleFacebookLogin} className="login-button-facebook">
+                                <FontAwesomeIcon icon={faFacebook} />
+                            </button>
+                            <button type="button" onClick={handleGitHubLogin} className="login-button-github">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </div>
         </div>
     );
 };
