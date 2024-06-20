e785acc65efb486cdc7a03309370ca6ca9acaeae
Ajtony2005 <79107320+Ajtony2005@users.noreply.github.com>
2024-06-20 21:34:40 +0200

    refactor: Add email field and registration toggle to login form

--- client/src/Login/jsx/login.jsx
+++ client/src/Login/jsx/login.jsx
@@ -1,15 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/login.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGoogle, faFacebook, faGithub } from '@fortawesome/free-brands-svg-icons';
 import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+import texts from '../json/login.json'; 
+import Cookies from 'js-cookie';
 
 const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
     const [showPassword, setShowPassword] = useState(false);
     const [isRegistering, setIsRegistering] = useState(false);
     const [email, setEmail] = useState('');
+    const [language, setLanguage] = useState('hu'); 
+
+    useEffect(() => {
+        const storedLanguage = Cookies.get('language');
+        if (storedLanguage) {
+            setLanguage(storedLanguage);
+        }
+
+        const interval = setInterval(() => {
+            const currentLanguage = Cookies.get('language');
+            if (currentLanguage && currentLanguage !== language) {
+                setLanguage(currentLanguage);
+            }
+        }, 1000); 
+
+        return () => clearInterval(interval);
+    }, [language]);
+
+    const getText = (key) => {
+        const keys = key.split('.');
+        let textValue = texts[language];
+        for (const k of keys) {
+            if (textValue[k] === undefined) {
+                return key; // Return the key as a fallback
+            }
+            textValue = textValue[k];
+        }
+        return textValue;
+    };
 
     const handleUsernameChange = (e) => {
         setUsername(e.target.value);
@@ -17,6 +49,10 @@ const Login = () => {
 
     const handlePasswordChange = (e) => {
         setPassword(e.target.value);
+    };
+
+    const handleConfirmPasswordChange = (e) => {
+        setConfirmPassword(e.target.value);
     };
 
     const handleEmailChange = (e) => {
@@ -41,6 +77,7 @@ const Login = () => {
             console.log('Email:', email);
             console.log('Felhasználónév:', username);
             console.log('Jelszó:', password);
+            console.log('Jelszó Újra:', confirmPassword);
         } else {
             console.log('Felhasználónév:', username);
             console.log('Jelszó:', password);
@@ -60,13 +97,13 @@ const Login = () => {
             <div className="login-container">
                 <div className="login-box">
                     <form onSubmit={handleSubmit}>
-                        <h1>{isRegistering ? 'Regisztráció' : 'Bejelentkezés'}</h1>
+                        <h1>{isRegistering ? getText('register.title') : getText('login.title')}</h1>
                         {isRegistering && (
                             <div>
                                 <input
                                     type="email"
                                     id="email"
-                                    placeholder="Email"
+                                    placeholder={getText('register.email')}
                                     className="login-input"
                                     value={email}
                                     onChange={handleEmailChange}
@@ -77,7 +114,7 @@ const Login = () => {
                             <input
                                 type="text"
                                 id="username"
-                                placeholder="Felhasználónév"
+                                placeholder={getText('login.username')}
                                 className="login-input"
                                 value={username}
                                 onChange={handleUsernameChange}
@@ -87,7 +124,7 @@ const Login = () => {
                             <input
                                 type={showPassword ? "text" : "password"}
                                 id="password"
-                                placeholder="Jelszó"
+                                placeholder={getText('login.password')}
                                 className="login-input"
                                 value={password}
                                 onChange={handlePasswordChange}
@@ -100,24 +137,42 @@ const Login = () => {
                                 <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
                             </button>
                         </div>
-                        <button type="submit" className="login-button">
-                            {isRegistering ? 'Regisztráció' : 'Bejelentkezés'}
-                        </button>
-                        
-                            <div className="login-buttons">
-                                <button type="button" onClick={handleGoogleLogin} className="login-button-google">
-                                    <FontAwesomeIcon icon={faGoogle} />
-                                </button>
-                                <button type="button" onClick={handleFacebookLogin} className="login-button-facebook">
-                                    <FontAwesomeIcon icon={faFacebook} />
-                                </button>
-                                <button type="button" onClick={handleGitHubLogin} className="login-button-github">
-                                    <FontAwesomeIcon icon={faGithub} />
+                        {isRegistering && (
+                            <div className="password-container">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    id="confirmPassword"
+                                    placeholder={getText('register.confirmPassword')}
+                                    className="login-input"
+                                    value={confirmPassword}
+                                    onChange={handleConfirmPasswordChange}
+                                />
+                                <button
+                                    type="button"
+                                    className="password-toggle-button"
+                                    onClick={togglePasswordVisibility}
+                                >
+                                    <FontAwesomeIcon icon={showPassword ? faEyeSlash : faEye} />
                                 </button>
                             </div>
+                        )}
+                        <button type="submit" className="login-button">
+                            {isRegistering ? getText('register.submit') : getText('login.submit')}
+                        </button>
+                        <div className="login-buttons">
+                            <button type="button" onClick={handleGoogleLogin} className="login-button-google">
+                                <FontAwesomeIcon icon={faGoogle} />
+                            </button>
+                            <button type="button" onClick={handleFacebookLogin} className="login-button-facebook">
+                                <FontAwesomeIcon icon={faFacebook} />
+                            </button>
+                            <button type="button" onClick={handleGitHubLogin} className="login-button-github">
+                                <FontAwesomeIcon icon={faGithub} />
+                            </button>
+                        </div>
                         <div className="register-link">
-                            <p>{isRegistering ? 'Már van fiókod? ' : 'Még nincs fiókod? '}
-                                <a href="#" onClick={toggleForm}>{isRegistering ? 'Bejelentkezés itt' : 'Regisztrálj itt'}</a>
+                            <p>{isRegistering ? getText('register.hasAccount') : getText('login.noAccount')} 
+                                <a href="#" onClick={toggleForm}>{isRegistering ? getText('register.loginHere') : getText('login.registerHere')}</a>
                             </p>
                         </div>
                     </form>
