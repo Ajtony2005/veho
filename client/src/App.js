82fe74befb68dfef7c9427332bd8df5fb85e3146
Ajtony2005 <79107320+Ajtony2005@users.noreply.github.com>
2024-07-13 23:17:15 +0200

    refactor: Update npm dependencies and configuration files

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,22 @@
-import React, { useEffect } from 'react';
-import Home from './Home/Home'
-import Cookies from 'js-cookie';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';  
-import Login from './Login/jsx/login.jsx';
+import React, { useEffect } from "react";
+import Home from "./Home/Home";
+import Cookies from "js-cookie";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Login/jsx/login.jsx";
+import Register from "./Login/jsx/register.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   useEffect(() => {
-    Cookies.set('language', 'hu', { expires: 30 }); 
+    Cookies.set("language", "hu", { expires: 30 });
   }, []);
-  
 
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </BrowserRouter>
   );
